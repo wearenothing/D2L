d405e31dc11e9096454b482e0e1c2b86322f7939
Sai <1391439267@qq.com>
2022-09-15 18:25:41 +0800

    Complete 3.2 Utilities

--- 3_Linear_Neural_Networks_for_Regression/3.2_OO_Design_for_Implementation.py
+++ 3_Linear_Neural_Networks_for_Regression/3.2_OO_Design_for_Implementation.py
@@ -4,16 +4,51 @@ import torch
 from torch import nn
 from d2l import torch as d2l
 
+# 1.Utilities
 def add_to_class(Class):
     def wrapper(obj):
-        setattr(Class,obj.__name__,obj)
+        setattr(Class, obj.__name__, obj)
     return wrapper
+
 class A:
     def __init__(self):
-        self.b=1
-a = A()
-@add_to_class(A)
-def do(self):
-    print('Class attribute "b" is',self.b)
+        self.b = 1
 
+a = A()
+
+@add_to_class(A)  # Add a method to class A
+def do(self):
+    print('Class attribute "b" is', self.b)
 a.do()
+
+
+class HyperParameters:  # @save
+    def save_hyperparameters(self, ignore=[]):
+        raise NotImplemented
+
+
+# Call the fully implemented HyperParameters class saved in d2l
+class B(d2l.HyperParameters):
+    def __init__(self, a, b, c):
+        self.save_hyperparameters(ignore=['c']) # Save function arguments into class attributes
+        print('self.a =', self.a, 'self.b =', self.b)
+        print('There is no self.c =', not hasattr(self, 'c'))
+b = B(a=1, b=2, c=3)
+
+# 2.Models
+
+class ProgressBoard(d2l.HyperParameters):  #@save
+    """Plot data points in animation."""
+    def __init__(self, xlabel=None, ylabel=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 ls=['-', '--', '-.', ':'], colors=['C0', 'C1', 'C2', 'C3'],
+                 fig=None, axes=None, figsize=(3.5, 2.5), display=True):
+        self.save_hyperparameters()
+
+    def draw(self, x, y, label, every_n=1):
+        raise NotImplemented
+
+board = d2l.ProgressBoard('x')
+for x in np.arange(0, 10, 0.1):
+    board.draw(x, np.sin(x), 'sin', every_n=2)
+    board.draw(x, np.cos(x), 'cos', every_n=10)
