cfbce1875c012af4b3423da6d7e3ee554afc3253
Sai <1391439267@qq.com>
2022-09-21 08:17:00 +0800

    Figure out "y.backward(torch.ones_like(x),retain_graph=True"

--- 5_Multilayer_Perceptrons/5.1_Multilayer_Perceptrons.py
+++ 5_Multilayer_Perceptrons/5.1_Multilayer_Perceptrons.py
@@ -6,3 +6,8 @@ y = torch.relu(x)
 # d2l.plot(x,y,'x','relu(x)',figsize=(5,2.5)) # Error
 d2l.plot(x.detach(),y.detach(),'x','relu(x)',figsize=(5,2.5))
 d2l.plt.show()
+
+y.backward(torch.ones_like(x),retain_graph=True)    # y.backward(w), compute the gradient of dot(y,w) to x,
+# perform the function as "y.sum().backward(retain_grad=True)"
+d2l.plot(x.detach(),x.grad,'x','grad of relu',figsize=(5,2.5))
+d2l.plt.show()
