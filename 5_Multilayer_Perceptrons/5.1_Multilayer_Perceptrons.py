9b9d26afe42d8151d736b245a7e5109ebe82fc8e
Sai <1391439267@qq.com>
2022-09-21 09:17:49 +0800

    5.1 Use matplotlib subplots

--- 5_Multilayer_Perceptrons/5.1_Multilayer_Perceptrons.py
+++ 5_Multilayer_Perceptrons/5.1_Multilayer_Perceptrons.py
@@ -1,13 +1,43 @@
 import torch
-from d2l import torch as d2l
+from matplotlib import pyplot as plt
 
+
+# 1.Relu
 x = torch.arange(-8,8,0.1,requires_grad=True)
 y = torch.relu(x)
+plt.figure(figsize=(9,6))
+plt.subplot(231)
 # d2l.plot(x,y,'x','relu(x)',figsize=(5,2.5)) # Error
-d2l.plot(x.detach(),y.detach(),'x','relu(x)',figsize=(5,2.5))
-d2l.plt.show()
+plt.plot(x.detach(),y.detach())
 
+
+# Gradient of Relu
 y.backward(torch.ones_like(x),retain_graph=True)    # y.backward(w), compute the gradient of dot(y,w) to x,
 # perform the function as "y.sum().backward(retain_grad=True)"
-d2l.plot(x.detach(),x.grad,'x','grad of relu',figsize=(5,2.5))
-d2l.plt.show()
+plt.subplot(234)
+plt.plot(x.detach(),x.grad)
+
+
+# 2.Sigmoid
+y = torch.sigmoid(x)
+plt.subplot(232)
+plt.plot(x.detach(),y.detach())
+
+# Gradient of sigmoid
+x.grad.zero_()
+y.backward(torch.ones_like(x),retain_graph=True)
+plt.subplot(235)
+plt.plot(x.detach(),x.grad)
+
+
+# 3.Tanh
+y = torch.tanh(x)
+
+plt.subplot(233)
+plt.plot(x.detach(),y.detach())
+
+x.grad.zero_()
+y.backward(torch.ones_like(x),retain_graph=True)
+plt.subplot(236)
+plt.plot(x.detach(),x.grad)
+plt.show()
