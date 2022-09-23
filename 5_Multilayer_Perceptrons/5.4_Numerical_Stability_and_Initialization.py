7ea1af05f2cd262e2bc95fa26ebfd434577bff49
Sai <1391439267@qq.com>
2022-09-23 17:21:33 +0800

    Complete 5.4 Numerical Stability and Initialization

--- 5_Multilayer_Perceptrons/5.4_Numerical_Stability_and_Initialization.py
+++ 5_Multilayer_Perceptrons/5.4_Numerical_Stability_and_Initialization.py
@@ -1,9 +1,18 @@
 import torch
 from d2l import torch as d2l
 
+# Vanishing Gradient
 x = torch.arange(-8,8,0.1,requires_grad=True)
 y = torch.sigmoid(x)
 y.backward(torch.ones_like(x))
 
 d2l.plot(x.detach(),[y.detach(),x.grad],legend=['sigmoid','gradient'],figsize=(6,4.5))
 d2l.plt.show()
+
+# Exploding Gradient
+M = torch.normal(0,1,size=(4,4))
+print('Before 100 times multiplying',M)
+for i in range(100):
+    M = M @ torch.normal(0,1,size=(4,4))
+
+print('After 100 times multiplying',M)
