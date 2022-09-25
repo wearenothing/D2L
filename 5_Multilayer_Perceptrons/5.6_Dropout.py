75d4445b524acd6d6231dacb9cc383dccbde0bd7
WangZhe <1391439267@qq.com>
2022-09-25 16:55:52 +0800

    Start 5.6 Dropout

---
+++ 5_Multilayer_Perceptrons/5.6_Dropout.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+def dropout_layer(X, dropout):
+    assert 0 <= dropout <= 1
+    if dropout == 1:
+        return torch.zeros_like(X)
+    else:
+        mask = (torch.rand(X.shape) > dropout).type(torch.float32)  # rand: uniform distribution
+        return mask * X / (1 - dropout)
+
+X = torch.arange(16, dtype = torch.float32).reshape((2, 8))
+print('dropout_p = 0:', dropout_layer(X, 0))
+print('dropout_p = 0.5:', dropout_layer(X, 0.5))
+print('dropout_p = 1:', dropout_layer(X, 1))
+
+class DropoutMLPScratch(d2l.Classifier):
+    def __init__(self,num_outputs,num_hiddens_1,num_hiddens_2,dropout_1,dropout_2,lr):
+        super(DropoutMLPScratch, self).__init__()
+        self.save_hyperparameters()
+        self.lin1 = nn.LazyLinear(num_hiddens_1)
+        self.lin2 = nn.LazyLinear(num_hiddens_2)
+        self.lin3 = nn.LazyLinear(num_outputs)
+        self.relu = nn.ReLU()
+
+
+    def forward(self,X):
+        H1 = self.relu(self.lin1(X))
+        if self.training:
+            H1 = dropout_layer(H1,self.dropout_1)
+        H2 = self.relu(self.lin2(H1))
+        if self.training:
+            H2 = dropout_layer(H2,self.dropout_2)
+        return self.lin3(H2)
+
+hparams = {'num_outputs':10, 'num_hiddens_1':256, 'num_hiddens_2':256,
+           'dropout_1':0.5, 'dropout_2':0.5, 'lr':0.1}
+model = DropoutMLPScratch(**hparams)
+data = d2l.FashionMNIST(batch_size=256)
+trainer = d2l.Trainer(max_epochs=10)
+trainer.fit(model, data)
+d2l.plt.show()
