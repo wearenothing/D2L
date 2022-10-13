acb06c02b3db65c1feb50bbcd936ec6951c5ee41
WangZhe <1391439267@qq.com>
2022-10-13 11:00:35 +0800

    Complete 9.3 Language Models

---
+++ 9_RNN/9.3_Language_Models.py
@@ -0,0 +1,22 @@
+import torch
+from d2l import torch as d2l
+
+@d2l.add_to_class(d2l.TimeMachine)  #@save
+def __init__(self, batch_size, num_steps, num_train=10000, num_val=5000):
+    super(d2l.TimeMachine, self).__init__()
+    self.save_hyperparameters()
+    corpus, self.vocab = self.build(self._download())
+    array = torch.tensor([corpus[i:i+num_steps+1]
+                        for i in range(0, len(corpus)-num_steps-1)])
+    self.X, self.Y = array[:,:-1], array[:,1:]
+
+@d2l.add_to_class(d2l.TimeMachine)  #@save
+def get_dataloader(self, train):
+    idx = slice(0, self.num_train) if train else slice(
+        self.num_train, self.num_train + self.num_val)
+    return self.get_tensorloader([self.X, self.Y], train, idx)
+
+data = d2l.TimeMachine(batch_size=2, num_steps=10)
+for X, Y in data.train_dataloader():
+    print('X:', X, '\nY:', Y)
+    break
